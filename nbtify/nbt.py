df519fd61cdf4e06fa541f1edf2e8368f1728346
Kella <kellador@gmail.com>
2017-10-28 22:50:51 +0200

    :hammer: rewrite nbt.py

--- nbtify/nbt.py
+++ nbtify/nbt.py
@@ -1,312 +1,367 @@
-from struct import unpack, pack
+from struct import pack, unpack
 import gzip
+from collections.abc import MutableMapping, MutableSequence
 
 
-def decode_name(decode, named=True):
-    if not named:
-        name = None
-    else:
-        name = decode.io.read(decode("H", 2)[0]).decode('utf-8')
-    return name
-
-
-def encode_name(self, encode):
-    if self.name is not None:
-        encode("H", len(self.name))
-        encode.io.write(self.name.encode('utf-8'))
-
-
-class TAG_End:
+class NBTException(Exception):
     pass
 
 
-class TAG_Byte:
-    def __init__(self, value=None, name=None, decode=None, named=True):
-        if decode is None:
+class TAGMixin:
+    def encode(self, io, fmt, *args):
+        io.write(pack(">" + fmt, *args))
+
+    def decode(self, io, fmt, size):
+        return unpack(">" + fmt, io.read(size))
+
+    def encode_name(self, io):
+        io.write(pack(">H", len(self.name)))
+        io.write(self.name.encode('utf-8'))
+
+    def decode_name(self, io):
+        return io.read(unpack(">H", 2)[0]).decode('utf-8')
+
+    def __str__(self):
+        return f'{self.__class__.name__} {self.name}: {self.value}'
+
+
+class TAG_End():
+    pass
+
+
+class TAG_Byte(TAGMixin):
+    def __init__(self, name=None, value=None, io=None):
+        if io is None:
             self.name = name
             self.value = value
         else:
-            self.name = decode_name(decode, named)
-            self.value = decode("b", 1)[0]
+            if name is not None:
+                self.name = name
+            self.value = self.decode(io, 'b', 1)[0]
 
-    def nbtify(self, encode, hastag=True):
-        if hastag:
-            encode("b", 1)
-        encode_name(self, encode)
-        encode("b", self.value)
-
-    def editValue(self, value):
-        self.value = int(value)
+    def saveNBT(self, io, typed=True):
+        if typed:
+            self.encode(io, 'b', 1)
+        if self.name is not None:
+            self.encode_name(io)
+        self.encode(io, 'b', self.value)
 
 
-class TAG_Short:
-    def __init__(self, value=None, name=None, decode=None, named=True):
-        if decode is None:
+class TAG_Short(TAGMixin):
+    def __init__(self, name=None, value=None, io=None):
+        if io is None:
             self.name = name
             self.value = value
         else:
-            self.name = decode_name(decode, named)
-            self.value = decode("h", 2)[0]
+            if name is not None:
+                self.name = name
+            self.value = self.decode(io, 'h', 2)[0]
 
-    def nbtify(self, encode, hastag=True):
-        if hastag:
-            encode("b", 2)
-        encode_name(self, encode)
-        encode("h", self.value)
-
-    def editValue(self, value):
-        self.value = int(value)
+    def saveNBT(self, io, typed=True):
+        if typed:
+            self.encode(io, 'b', 2)
+        if self.name is not None:
+            self.encode_name(io)
+        self.encode(io, 'h', self.value)
 
 
-class TAG_Int:
-    def __init__(self, value=None, name=None, decode=None, named=True):
-        if decode is None:
+class TAG_Int(TAGMixin):
+    def __init__(self, name=None, value=None, io=None):
+        if io is None:
             self.name = name
             self.value = value
         else:
-            self.name = decode_name(decode, named)
-            self.value = decode("i", 4)[0]
+            if name is not None:
+                self.name = name
+            self.value = self.decode(io, 'i', 4)[0]
 
-    def nbtify(self, encode, hastag=True):
-        if hastag:
-            encode("b", 3)
-        encode_name(self, encode)
-        encode("i", self.value)
-
-    def editValue(self, value):
-        self.value = int(value)
+    def saveNBT(self, io, typed=True):
+        if typed:
+            self.encode(io, 'b', 3)
+        if self.name is not None:
+            self.encode_name(io)
+        self.encode(io, 'i', self.value)
 
 
-class TAG_Long:
-    def __init__(self, value=None, name=None, decode=None, named=True):
-        if decode is None:
+class TAG_Long(TAGMixin):
+    def __init__(self, name=None, value=None, io=None):
+        if io is None:
             self.name = name
             self.value = value
         else:
-            self.name = decode_name(decode, named)
-            self.value = decode("q", 8)[0]
+            if name is not None:
+                self.name = name
+            self.value = self.decode(io, 'q', 8)[0]
 
-    def nbtify(self, encode, hastag=True):
-        if hastag:
-            encode("b", 4)
-        encode_name(self, encode)
-        encode("q", self.value)
-
-    def editValue(self, value):
-        self.value = int(value)
+    def saveNBT(self, io, typed=True):
+        if typed:
+            self.encode(io, 'b', 4)
+        if self.name is not None:
+            self.encode_name(io)
+        self.encode(io, 'q', self.value)
 
 
-class TAG_Float:
-    def __init__(self, value=None, name=None, decode=None, named=True):
-        if decode is None:
+class TAG_Float(TAGMixin):
+    def __init__(self, name=None, value=None, io=None):
+        if io is None:
             self.name = name
             self.value = value
         else:
-            self.name = decode_name(decode, named)
-            self.value = decode("f", 4)[0]
+            if name is not None:
+                self.name = name
+            self.value = self.decode(io, 'f', 4)[0]
 
-    def nbtify(self, encode, hastag=True):
-        if hastag:
-            encode("b", 5)
-        encode_name(self, encode)
-        encode("f", self.value)
-
-    def editValue(self, value):
-        self.value = float(value)
+    def saveNBT(self, io, typed=True):
+        if typed:
+            self.encode(io, 'b', 5)
+        if self.name is not None:
+            self.encode_name(io)
+        self.encode(io, 'f', self.value)
 
 
-class TAG_Double:
-    def __init__(self, value=None, name=None, decode=None, named=True):
-        if decode is None:
+class TAG_Double(TAGMixin):
+    def __init__(self, name=None, value=None, io=None):
+        if io is None:
             self.name = name
             self.value = value
         else:
-            self.name = decode_name(decode, named)
-            self.value = decode("d", 8)[0]
+            if name is not None:
+                self.name = name
+            self.value = self.decode(io, 'd', 8)[0]
 
-    def nbtify(self, encode, hastag=True):
-        if hastag:
-            encode("b", 6)
-        encode_name(self, encode)
-        encode("d", self.value)
-
-    def editValue(self, value):
-        self.value = float(value)
+    def saveNBT(self, io, typed=True):
+        if typed:
+            self.encode(io, 'b', 6)
+        if self.name is not None:
+            self.encode_name(io)
+        self.encode(io, 'd', self.value)
 
 
-class TAG_String:
-    def __init__(self, value=None, name=None, decode=None, named=True):
-        if decode is None:
+class TAG_Byte_Array(MutableSequence, TAGMixin):
+    def __init__(self, name=None, value=None, io=None):
+        if io is None:
             self.name = name
             self.value = value
         else:
-            self.name = decode_name(decode, named)
-            length = decode("H", 2)[0]
-            self.value = decode.io.read(length).decode('utf-8')
+            if name is not None:
+                self.name = name
+            self.value = bytearray(io.read(self.decode(io, 'i', 4)[0]))
 
-    def nbtify(self, encode, hastag=True):
-        if hastag:
-            encode("b", 8)
-        encode_name(self, encode)
-        encode("H", len(self.value.encode('utf-8')))
-        encode.io.write(self.value.encode('utf-8'))
+    def saveNBT(self, io, typed=True):
+        if typed:
+            self.encode(io, 'b', 7)
+        if self.name is not None:
+            self.encode_name(io)
+        self.encode(io, 'i', len(self.value))
+        for i in self.value:
+            self.encode(io, 'b', i)
 
-    def editValue(self, value):
-        self.value = value
+    def __getitem__(self, index):
+        return self.value[index]
+
+    def __setitem__(self, index, value):
+        self.value[index] = value
+
+    def __delitem__(self, index):
+        del self.value[index]
+
+    def __len__(self):
+        return len(self.value)
+
+    def insert(self, index, value):
+        self.value.insert(index, value)
+
+    def __str__(self):
+        return f'{self.__class__.__name__}: {self.name}: ' + \
+            '[' + ' '.join([str(x) for x in self.value]) + ']'
 
 
-class TAG_List:
-    def __init__(self, value=None, name=None, decode=None, named=True, tags_type=None, list_size=None):
-        if decode is None:
+class TAG_String(TAGMixin):
+    def __init__(self, name=None, value=None, io=None):
+        if io is None:
+            self.name = name
+            self.value = value
+        else:
+            if name is not None:
+                self.name = name
+            self.value = self.decode_name(io)
+
+    def saveNBT(self, io, typed=True):
+        if typed:
+            self.encode(io, 'b', 8)
+        if self.name is not None:
+            self.encode_name(io)
+        io.write(pack(">H", len(self.value.encode('utf-8'))))
+        io.write(self.value.encode('utf-8'))
+
+
+class TAG_List(MutableSequence, TAGMixin):
+    def __init__(self, name=None, value=None, tags_type=None, io=None):
+        if io is None:
             self.name = name
             self.value = value
             self.tags_type = tags_type
-            self.list_size = list_size
         else:
-            self.name = decode_name(decode, named)
-            self.tags_type = decode("b", 1)[0]
-            self.list_size = decode("i", 4)[0]
-            taglist = []
-            for i in range(self.list_size):
-                taglist.append(tag[self.tags_type](decode=decode, named=False))
-            self.value = taglist
+            if name is not None:
+                self.name = name
+            self.value = []
+            self.tags_type = self.decode(io, 'b', 1)[0]
+            for _ in range(self.decode(io, 'i', 4)[0]):
+                self.value.append(tag[self.tags_type](io=io))
 
-    def nbtify(self, encode, hastag=True):
-        if hastag:
-            encode("b", 9)
-        encode_name(self, encode)
-        encode("b", self.tags_type)
-        encode("i", self.list_size)
-        for i in self.value:
-            i.nbtify(encode, hastag=False)
+    def saveNBT(self, io, typed=True):
+        if typed:
+            self.encode(io, 'b', 9)
+        if self.name is not None:
+            self.encode_name(io)
+        if len(self.value) > 0:
+            self.encode(io, 'b', self.tags_type)
+            self.encode(io, 'i', len(self.value))
+            for i in self.value:
+                i.saveNBT(io, typed=False)
+        else:
+            self.encode(io, 'b', 0)
+            self.encode(io, 'i', 0)
 
-    def addTAG(self, value):
-        self.value.append(tag[self.tags_type](value=value))
+    def __getitem__(self, index):
+        return self.value[index]
+
+    def __setitem__(self, index, value):
+        self.value[index] = value
+
+    def __delitem__(self, index):
+        del self.value[index]
+
+    def __len__(self):
+        return len(self.value)
+
+    def insert(self, index, value):
+        self.value.insert(index, value)
+
+    def __str__(self):
+        return f'{self.__class__.__name__}: {self.name}: ' + \
+            '[\n\t' + '\n\t'.join([str(x) for x in self.value]) + ']'
 
 
-class TAG_Compound:
-    def __init__(self, value=None, name=None, decode=None, named=True):
-        if decode is None:
+class TAG_Compound(MutableMapping, TAGMixin):
+    def __init__(self, name=None, value=None, io=None):
+        if io is None:
             self.name = name
             self.value = value
         else:
-            self.name = decode_name(decode, named)
-            compound = []
+            if name is not None:
+                self.name = name
+            self.value = {}
             while True:
-                _type = decode("b", 1)[0]
-                if _type == 0:
+                typeID = self.decode(io, 'b', 1)[0]
+                if typeID == 0:
                     break
-                compound.append(tag[_type](decode=decode))
-            self.value = compound
+                tagName = self.decode_name(io)
+                self.value[tagName] = tag[typeID](name=tagName, io=io)
 
-    def nbtify(self, encode=None, hastag=True, io=None):
-        if io is not None:
-            encode = lambda fmt, *args: io.write(pack(">" + fmt, *args))
-            encode.io = io
-        if hastag:
-            encode("b", 10)
-        encode_name(self, encode)
+    def saveNBT(self, io, typed=True):
+        if typed:
+            self.encode(io, 'b', 10)
+        if self.name is not None:
+            self.encode_name(io)
         for i in self.value:
-            i.nbtify(encode)
-        encode("b", 0)
+            i.saveNBT(io)
+        self.encode(io, 'b', 0)
 
-    def addTAG(self, tagtype, name):
-        self.value.append(tag[tagtype](name=name))
+    def __getitem__(self, key):
+        return self.value[key]
+
+    def __setitem__(self, key, value):
+        self.value[key] = value
+
+    def __delitem__(self, key):
+        del self.value[key]
+
+    def __iter__(self):
+        return iter(self.value)
+
+    def __len__(self):
+        return len(self.value)
+
+    def __str__(self):
+        return f'{self.__class__.__name__}: {self.name}: ' + \
+            '{' + '\n\t'.join([str(x) for x in self.value]) + '}'
 
 
-class TAG_Int_Array:
-    def __init__(self, value=None, name=None, decode=None, named=True):
-        if decode is None:
+class TAG_Int_Array(MutableSequence, TAGMixin):
+    def __init__(self, name=None, value=None, io=None):
+        if io is None:
             self.name = name
             self.value = value
         else:
-            self.name = decode_name(decode, named)
-            arlen = decode("i", 4)
-            arlen = arlen[0]
-            if arlen == 0:
-                self.value = []
-            else:
-                self.value = []
-                while arlen > 0:
-                    self.value.append(decode("i", 4)[0])
-                    arlen -= 1
+            if name is not None:
+                self.name = name
+            self.value = []
+            for _ in range(self.decode(io, 'i', 4)[0]):
+                self.value.append(self.decode(io, 'i', 4)[0])
 
-    def nbtify(self, encode, hastag=True):
-        if hastag:
-            encode("b", 11)
-        encode_name(self, encode)
-        encode("i", len(self.value))
-        if len(self.value) != 0:
-            for n in self.value:
-                encode("i", n)
-
-
-class TAG_Byte_Array:
-    def __init__(self, value=None, name=None, decode=None, named=True):
-        if decode is None:
-            self.name = name
-            self.value = value
-        else:
-            self.name = decode_name(decode, named)
-            self.value = bytearray(decode.io.read(decode("i", 4)[0]))
-
-    def nbtify(self, encode, hastag=True):
-        if hastag:
-            encode("b", 7)
-        encode_name(self, encode)
-        encode("i", len(self.value))
+    def saveNBT(self, io, typed=True):
+        if typed:
+            self.encode(io, 'b', 11)
+        if self.name is not None:
+            self.encode_name(io)
         for i in self.value:
-            encode("b", i)
+            self.encode(io, 'i', i)
+
+    def __getitem__(self, index):
+        return self.value[index]
+
+    def __setitem__(self, index, value):
+        self.value[index] = value
+
+    def __delitem__(self, index):
+        del self.value[index]
+
+    def __len__(self):
+        return len(self.value)
+
+    def insert(self, index, value):
+        self.value.insert(index, value)
+
+    def __str__(self):
+        return f'{self.__class__.__name__}: {self.name}: ' + \
+            '[' + ' '.join([str(x) for x in self.value]) + ']'
 
 
 class NBTObj(TAG_Compound):
-    def __init__(self, nbtfile):
-        self.nbtfile = nbtfile
-        try:
-            with gzip.open(nbtfile, 'rb') as io:
-                decode = lambda fmt, size: unpack(">" + fmt, io.read(size))
-                decode.io = io
-                if decode("b", 1)[0] != 10:
-                    raise NBTException("Invalid NBT data! Maybe it's gzipped?")
-                super().__init__(decode=decode)
-                io.close()
-            self.gzipped = True
-        except OSError:
-            with open(nbtfile, 'rb') as io:
-                decode = lambda fmt, size: unpack(">" + fmt, io.read(size))
-                decode.io = io
-                if decode("b", 1)[0] != 10:
-                    raise NBTException("Invalid NBT data! Maybe it's gzipped?")
-                super().__init__(decode=decode)
-                io.close()
-            self.gzipped = False
+    def __init__(self, nbtfile=None):
+        if nbtfile is not None:
+            self.nbtfile = nbtfile
+            try:
+                with gzip.open(self.nbtfile, 'rb') as io:
+                    if self.decode(io, 'b', 1)[0] != 10:
+                        raise NBTException("Invalid NBT File!")
+                    super().__init__(io=io)
+                self.gzipped = True
+            except OSError:
+                with open(self.nbtfile, 'rb') as io:
+                    if self.decode(io, 'b', 1)[0] != 10:
+                        raise NBTException("Invalid NBT File!")
+                    super().__init__(io=io)
+                self.gzipped = False
 
     def reload(self):
-        try:
+        if self.gzipped:
             with gzip.open(self.nbtfile, 'rb') as io:
                 super().__init__(io=io)
-                io.close()
-            self.gzipped = True
-        except OSError:
+        else:
             with open(self.nbtfile, 'rb') as io:
                 super().__init__(io=io)
-                io.close()
-            self.gzipped = False
 
     def save(self, destfile=None):
         if destfile is None:
-            nbtfile = self.nbtfile
-        else:
-            nbtfile = destfile
+            destfile = self.nbtfile
         if self.gzipped:
-            with gzip.open(nbtfile, 'wb') as io:
-                self.nbtify(io=io)
-                io.close()
+            with gzip.open(destfile, 'wb') as io:
+                super().saveNBT(io)
         else:
-            with open(nbtfile, 'wb') as io:
-                self.nbtify(io=io)
-                io.close()
-        return True
+            with open(destfile, 'wb') as io:
+                super().saveNBT(io)
 
 
 tag = {
@@ -323,23 +378,3 @@ tag = {
     10: TAG_Compound,
     11: TAG_Int_Array
 }
-
-
-tagString = {
-    TAG_End: 'End',
-    TAG_Byte: 'Byte',
-    TAG_Short: 'Short',
-    TAG_Int: 'Int',
-    TAG_Long: 'Long',
-    TAG_Float: 'Float',
-    TAG_Double: 'Double',
-    TAG_Byte_Array: 'Byte_Array',
-    TAG_String: 'String',
-    TAG_List: 'List',
-    TAG_Compound: 'Compound',
-    TAG_Int_Array: 'Int_Array'
-}
-
-
-class NBTException(Exception):
-    pass
