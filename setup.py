dc84c24ef6001ad7530aed3bdf14fd16697d0bca
Kellador <kellador@gmail.com>
2021-01-25 16:12:47 +0100

    :bookmark: bump version to beta since everything actually works now...

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='yonbt',
-    version='1.0.0a1',
+    version='1.0.0b1',
     url='https://github.com/Kellador/YoNBT',
     packages=['yonbt'],
     install_requires=[
@@ -14,7 +14,7 @@ setup(
     description='A python library for Mojang\'s NBT file format and its various uses.',
     keywords='nbt minecraft-nbt minecraft-region',
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 2 - Beta',
         'License :: OSI Approved :: MIT License',
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Libraries',
