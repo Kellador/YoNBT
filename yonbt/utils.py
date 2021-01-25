715760afc4935e7368f97ec94a5cfad2438c49ac
Kellador <kellador@gmail.com>
2021-01-25 16:11:49 +0100

    :zap: simplify utilites

--- yonbt/utils.py
+++ yonbt/utils.py
@@ -1,41 +1,19 @@
 import logging
-from .region import Region
+
+from typing import Tuple
 
 log = logging.getLogger(__name__)
 
 
-def chunkByBlock(x, z):
+def chunkByBlock(x: int, z: int) -> Tuple[int, int]:
     return (x >> 4), (z >> 4)
 
 
-def regionByBlock(x, z):
-    return 'r.' + str((x >> 4) // 32) + '.' + str((z >> 4) // 32) + '.mca'
+def locateBlock(x: int, z: int) -> Tuple[str, Tuple[int, int]]:
+    region = 'r.' + str((x >> 4) // 32) + '.' + str((z >> 4) // 32) + '.mca'
+    chunk = (x >> 4), (z >> 4)
+    return region, chunk
 
 
-def regionByChunk(x, z):
+def locateChunk(x: int, z: int) -> str:
     return 'r.' + str(x // 32) + '.' + str(z // 32) + '.mca'
-
-
-def getTileEntity(x, z, obj):
-    if isinstance(obj, Region):
-        chunk = obj[chunkByBlock(x, z)]
-    else:
-        chunk = obj
-    entities = chunk.nbt['Level']['TileEntities']
-    if len(entities) > 0:
-        for e in entities:
-            if e['x'].value == x and e['z'].value == z:
-                return e, entities
-        else:
-            log.info(f'No TileEntity at {x},{z} in given chunk!')
-            return None
-    else:
-        log.info('No TileEntities in given chunk!')
-        return None
-
-
-def deleteTileEntity(x, z, obj):
-    e = getTileEntity(x, z, obj)
-    if e is not None:
-        e[1].remove(e[0])
-        log.info(f'TileEntity at {x},{z} removed!')
